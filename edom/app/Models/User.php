994de2840f83e9a68fa010b7d85ec5c1ee95afac
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2024-11-05 08:26:31 +0700

    Base Model

--- edom/app/Models/User.php
+++ edom/app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function evaluations() {
+        return $this->hasMany(Evaluation::class);
+    }
 }
